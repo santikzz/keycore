1c44d178582e1365d0cadf788650fc6bf29020eb
santi <574373472+@santi.github.com>
2025-07-28 00:19:10 -0300

    build

---
+++ public/build/assets/index-B3xlNdz0.js
@@ -0,0 +1,1 @@
+import{v as r,w as t}from"./app-dwFro-sa.js";var a=r();const e=t(a);export{e as R,a as r};
---
+++ public/build/assets/input-error-CV-mlL2K.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-dwFro-sa.js";import{a as o}from"./button-B6mWijBL.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
